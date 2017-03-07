f00d0fef7c8e6d44ae2bfb4e31101eebcadd80cf
1408143 <1408143@rgu.ac.uk>
2017-03-07 15:58:25 +0000

    fdc

--- dbConnect.php
+++ dbConnect.php
@@ -14,13 +14,12 @@
         if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
             continue;
         }
-        $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "1", $value);
-        $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "1", $value);
-        $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "1", $value);
-        $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "1", $value);
+        $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+        $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+        $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+        $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
     }
-    //$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, $connectstr_dbname);
-    $link = mysqli_connect("MYSQLCONNSTR_localdb", "root", "", "localdb");
+    $link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, $connectstr_dbname);
     if (!$link) {
         echo "Error: Unable to connect to MySQL." . PHP_EOL;
         echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
