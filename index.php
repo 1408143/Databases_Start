6b54a8fe4ee62816f2eec75d89c7fb7eeecc0953
1408143 <1408143@rgu.ac.uk>
2017-03-07 16:29:33 +0000

    fdc

--- index.php
+++ index.php
@@ -5,7 +5,26 @@
  * Date: 07/03/2017
  * Time: 14:52
  */
-include "dbConnect.php";
+
 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Links Links Links</title>
+    </head>
+    <body>
+    <header>
+        <div id="quicklinks">
+            <ul>
+                <li><a href="response.php?query=all">Display All Movies</a></li>
+                <li><a href="response.php?query=marvel">Display Movies created by Marvel Studios</a></li>
+                <li><a href="response.php?query=2010">Display all movies created after 2010</a></li>
+                <li><a href="response.php?query=xmen">Display all X-Men films</a></li>
+            </ul>
+        </div>
+    </header>
+    </body>
+</html>
---
+++ response.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1408143
+ * Date: 07/03/2017
+ * Time: 16:13
+ */
+#
+include "dbConnect.php";
+$query= $_GET["query"];
+
+switch($query){
+    default:
+        echo "Nothing selected!";
+        break;
+    case "all":
+        $q="SELECT * FROM marvelmovies";
+        $result=$link->query($q);
+        while($row = $result->fetch_array()){
+            echo $row("title")."</br>";
+        }
+        $result->close();
+        break;
+    case "marvel":
+        $q="SELECT * FROM marvelmovies WHERE productionStudio LIKE 'Marvel Studios'";
+        $result=$link->query($q);
+        $result->close();
+        break;
+    case "2010":
+        $q="SELECT * FROM marvelmovies WHERE yearReleased<=2010";
+        $result=$link->query($q);
+        $result->close();
+        break;
+    case "xmen":
+        $q="SELECT * FROM marvelmovies WHERE  title LIKE '%X-Men%'";
+        $result=$link->query($q);
+        $result->close();
+        break;
+}
+$link->close();
+?>
